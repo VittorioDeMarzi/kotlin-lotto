090e5f6ed954753afbafcdbb5d0082778cb73da6
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-02 12:38:30 +0200

    refactor:(LottoLogic) generating list of unique numbers

--- src/main/kotlin/LottoLogic.kt
+++ src/main/kotlin/LottoLogic.kt
@@ -8,8 +8,17 @@ class LottoLogic {
     }
 
     fun generateListOfSixRandomNumbers(): List<Int> {
-        val listOfNumbers = List(size = 6) { Random.nextInt(50)}
+        val listOfNumbers = (1..50).shuffled().take(6)
         return listOfNumbers
+    }
+
+    fun generateTickets(numberOfTickets: Int): List<Lotto> {
+        val listOfTickets = mutableListOf<Lotto>()
+        repeat(numberOfTickets) {
+            val lotto = Lotto(generateListOfSixRandomNumbers())
+            listOfTickets.add(lotto)
+        }
+        return listOfTickets
     }
 
 }
