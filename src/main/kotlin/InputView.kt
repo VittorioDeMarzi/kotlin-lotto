04629e2fe39ea22c62258d9339a1f6ca8692b980
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-02 10:45:56 +0200

    feat: update InputView

--- src/main/kotlin/InputView.kt
+++ src/main/kotlin/InputView.kt
@@ -1,16 +1,19 @@
 class InputView {
 
     fun purchaseAmountInput(): String? {
+        println("Please enter the purchase amount.")
         val purchaseAmount = readlnOrNull()
         return purchaseAmount
     }
 
     fun winningNumbersInput(): String? {
+        println("Please enter last week’s winning numbers.")
         val winningNumbers = readlnOrNull()
         return winningNumbers
     }
 
     fun bonusNumberInput(): String? {
+        println("Please enter the bonus number.")
         val bonusNumber = readlnOrNull()
         return bonusNumber
     }
