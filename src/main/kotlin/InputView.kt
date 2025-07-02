cb91132b1a2a0eded5523f5bd5a72f21165d3962
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-02 10:41:11 +0200

    feat: InputView implementation

--- src/main/kotlin/InputView.kt
+++ src/main/kotlin/InputView.kt
@@ -1,5 +1,17 @@
 class InputView {
 
+    fun purchaseAmountInput(): String? {
+        val purchaseAmount = readlnOrNull()
+        return purchaseAmount
+    }
 
+    fun winningNumbersInput(): String? {
+        val winningNumbers = readlnOrNull()
+        return winningNumbers
+    }
 
+    fun bonusNumberInput(): String? {
+        val bonusNumber = readlnOrNull()
+        return bonusNumber
+    }
 }
