77f726c143b0327958ca2c556a35e2f8a8c1c142
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-02 12:44:43 +0200

    test: generate right number of tickets

--- src/test/kotlin/LottoLogicTest.kt
+++ src/test/kotlin/LottoLogicTest.kt
@@ -7,7 +7,7 @@ class LottoLogicTest {
     val lottoLogic = LottoLogic()
 
     @Test
-    fun `calculate the right NumberOfTickets`(){
+    fun `calculate the right number of tickets`(){
         val purchaseAmount = 10000
         val rightNumberOfTickets = purchaseAmount/1000
         val numberOfTickets = lottoLogic.calculateNumberOfTickets(purchaseAmount)
@@ -19,5 +19,11 @@ class LottoLogicTest {
         val listOfNumbers = lottoLogic.generateListOfSixRandomNumbers()
         assertThat(listOfNumbers).hasSize(6)
     }
-  
+
+    @Test
+    fun `generate the right number of tickets`() {
+        val numberOfTickets = 6
+        val tickets = lottoLogic.generateTickets(numberOfTickets)
+        assertThat(tickets).hasSize(numberOfTickets)
+    }
 }
