ecbbe6a113f310ca654e04271bbd9c1fdabcc12e
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-03 14:40:11 +0200

    refactor(Rank): use countOfMatch property

--- src/main/kotlin/Rank.kt
+++ src/main/kotlin/Rank.kt
@@ -4,18 +4,26 @@ enum class Rank(val countOfMatch: Int, val winningMoney: Int) {
     THIRD(5, 1_500_000),
     FOURTH(4, 50_000),
     FIFTH(3, 5_000),
-    MISS(0, 0);
+    MISS(0, 0),
+    ;
 
     companion object {
-        fun valueOf(countOfMatch: Int, matchBonus: Boolean): Rank {
-          return when {
-              countOfMatch == 6 -> FIRST
-              countOfMatch == 5 && matchBonus -> SECOND
-              countOfMatch == 5 -> THIRD
-              countOfMatch == 4 -> FOURTH
-              countOfMatch == 3 -> FIFTH
-              else -> MISS
-          }
+        fun valueOf(
+            countOfMatch: Int,
+            matchBonus: Boolean,
+        ): Rank {
+            return when {
+                countOfMatch == FIRST.countOfMatch -> FIRST
+                countOfMatch == SECOND.countOfMatch && matchBonus -> SECOND
+                countOfMatch == THIRD.countOfMatch -> THIRD
+                countOfMatch == FOURTH.countOfMatch -> FOURTH
+                countOfMatch == FIFTH.countOfMatch -> FIFTH
+                else -> MISS
+            }
         }
+    }
+
+    fun display(): String {
+        return "${this.countOfMatch} Matches (${this.winningMoney})"
     }
 }
