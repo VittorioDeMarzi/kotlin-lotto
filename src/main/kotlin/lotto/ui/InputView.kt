237377c1c155b4e381856d271a7ae063f7323886
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-03 16:53:42 +0200

    refactor(InputView): throw an error with invalid input

--- src/main/kotlin/lotto/ui/InputView.kt
+++ src/main/kotlin/lotto/ui/InputView.kt
@@ -4,19 +4,19 @@ object InputView {
     fun purchaseAmountInput(): Int {
         println("Please enter the purchase amount.")
         val purchaseAmount =
-            readln().toIntOrNull() ?: throw IllegalArgumentException("The purchase amount has to be a valid number.")
+            readln().toIntOrNull() ?: throw IllegalArgumentException("Invalid input")
         return purchaseAmount
     }
 
-    fun winningNumbersInput(): String? {
+    fun winningNumbersInput(): String {
         println("Please enter last week’s winning numbers.")
-        val winningNumbers = readlnOrNull()
+        val winningNumbers = readlnOrNull() ?: throw IllegalArgumentException("Invalid input")
         return winningNumbers
     }
 
-    fun bonusNumberInput(): String? {
+    fun bonusNumberInput(): Int {
         println("Please enter the bonus number.")
-        val bonusNumber = readlnOrNull()
+        val bonusNumber = readln().toIntOrNull() ?: throw IllegalArgumentException("Invalid input")
         return bonusNumber
     }
 }
