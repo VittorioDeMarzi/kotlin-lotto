779e1ff6c8afc8bf1470fed7f8328eb1cbf38574
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-04 15:32:15 +0200

    test(InputValidation): parse purchase amount correctly

--- src/test/kotlin/lotto/core/InputValidationTest.kt
+++ src/test/kotlin/lotto/core/InputValidationTest.kt
@@ -5,18 +5,23 @@ import org.junit.jupiter.api.assertThrows
 import kotlin.test.Test
 
 class InputValidationTest {
+    // Purchase Validation Test
+    @Test
+    fun `should throw if less then 1000`() {
+        val number = "999"
+        assertThrows<IllegalArgumentException> { InputValidation.validatePurchaseAmount(number) }
+    }
+
     @Test
     fun `winning numbers should create a list of numbers`() {
-        val inputValidation = InputValidation()
         val winningLottoString = "1,2,3,4,5,6"
-        val winningLotto = inputValidation.createWinningLottoList(winningLottoString)
+        val winningLotto = InputValidation.createWinningLottoList(winningLottoString)
         assertThat(winningLotto).hasSize(6)
     }
 
     @Test
     fun `winning numbers should has to be exactly 6 numbers`() {
-        val inputValidation = InputValidation()
         val winningLottoString = "1,2,3,4,5"
-        assertThrows<IllegalArgumentException> { inputValidation.createWinningLottoList(winningLottoString) }
+        assertThrows<IllegalArgumentException> { InputValidation.createWinningLottoList(winningLottoString) }
     }
 }
