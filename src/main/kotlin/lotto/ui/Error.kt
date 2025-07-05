54dab132085f7a4d794b8b9fd639a4127454c1df
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-05 18:29:50 +0200

    Refactor: Error messages

--- src/main/kotlin/lotto/ui/Error.kt
+++ src/main/kotlin/lotto/ui/Error.kt
@@ -1,10 +1,10 @@
 package lotto.ui
 
 enum class Error(val message: String) {
-    BONUS("BONUS NUMBER MUST NOT BE IN THE WINNING LOTTO"),
-    LOW_PURCHASE_AMOUNT("PURCHASE AMOUNT MUST ABOVE OR EQUAL TO 1000"),
-    NOT_DIVISIBLE_BY_1000("PURCHASE AMOUNT MUST DIVISIBLE BY 1000"),
-    INVALID_INPUT("INVALID INPUT"),
+    BONUS("[ERROR] Bonus number must not be among the winning numbers."),
+    LOW_PURCHASE_AMOUNT("[ERROR] The purchase amount must be greater or equal than 1000."),
+    NOT_DIVISIBLE_BY_1000("[ERROR] The purchase amount must be divisible by 1,000."),
+    INVALID_INPUT("[ERROR] Invalid Input"),
     INVALID_WINNING_SIZE("[ERROR] Winning numbers must be 6 numbers."),
     DUPLICATE_WINNING("[ERROR] Winning numbers must be unique."),
     OUT_OF_RANGE("[ERROR] Numbers must be between 1 and 45."),
