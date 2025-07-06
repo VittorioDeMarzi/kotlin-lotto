cbb2b0f288b4c5e84f04d587b95d11813252b34e
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-06 17:20:18 +0200

    refactor: Error messages

--- src/main/kotlin/lotto/view/Error.kt
+++ src/main/kotlin/lotto/view/Error.kt
@@ -8,4 +8,5 @@ enum class Error(val message: String) {
     INVALID_LOTTO_SIZE("[ERROR] Winning numbers must be 6 numbers."),
     DUPLICATE_WINNING("[ERROR] Winning numbers must be unique."),
     OUT_OF_RANGE("[ERROR] Numbers must be between 1 and 45."),
+    BONUS_OUT_OF_RANGE("[ERROR] Bonus must be between 1 and 45."),
 }
