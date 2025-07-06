d5b9acc045d9e5d3ef520240dc61847c789537a6
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-06 11:16:21 +0200

    refactor(Lotto): remove requirement. numbers is already a set.

--- src/main/kotlin/lotto/core/Lotto.kt
+++ src/main/kotlin/lotto/core/Lotto.kt
@@ -6,8 +6,7 @@ data class Lotto(private val numbers: Set<Int>) {
     constructor(numbers: List<Int>) : this(numbers.toSet())
 
     init {
-        require(numbers.toSet().size == numbers.size) { lotto.ui.Error.DUPLICATE_WINNING.message } // Numbers must be unique
-        require(numbers.all { it in 1..50 }) { lotto.ui.Error.OUT_OF_RANGE.message } // any number must be in range 1 - 50
+        require(numbers.all { it in 1..50 }) { Error.OUT_OF_RANGE.message } // any number must be in range 1 - 50
         require(numbers.size == 6) { Error.INVALID_WINNING_SIZE.message } // Winning lotto has to be 6 numbers
     }
 
