37143053369847ffbf2890d0dcc3fbf94a6f4c68
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-06 17:09:20 +0200

    feat(model): add WinningLotto class to encapsulate winning numbers and bonus logic

--- src/main/kotlin/lotto/model/WinningLotto.kt
+++ src/main/kotlin/lotto/model/WinningLotto.kt
@@ -2,8 +2,15 @@ package lotto.model
 
 import lotto.view.Error
 
-class WinningLotto(val lotto: Lotto, val bonusNumber: Int) {
+data class WinningLotto(private val lotto: Lotto, private val bonusNumber: Int) {
     init {
         require(!lotto.containsBonus(bonusNumber)) { throw IllegalArgumentException(Error.BONUS.message) }
+        require(bonusNumber in 1..45) { Error.BONUS_OUT_OF_RANGE }
     }
+
+    fun countMatches(other: Lotto): Int {
+        return lotto.countMatches(other)
+    }
+
+    fun isBonusMatched(lotto: Lotto): Boolean = lotto.containsBonus(bonusNumber)
 }
