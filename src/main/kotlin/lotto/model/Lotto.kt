6cf89b2165a9ac3f2a3de7463cc2cce5116b3660
Vittorio De Marzi <vittorio.demarzi90@gmail.com>
2025-07-07 14:04:38 +0200

    feat(model): add LottoNumber class to wrap a single Int value

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -2,11 +2,8 @@ package lotto.model
 
 import lotto.view.Error
 
-data class Lotto(private val numbers: Set<Int>) {
-    constructor(numbers: List<Int>) : this(numbers.toSet())
-
+data class Lotto(private val numbers: Set<LottoNumber>) {
     init {
-        require(numbers.all { it in 1..50 }) { Error.OUT_OF_RANGE.message } // any number must be in range 1 - 50
         require(numbers.size == 6) { Error.INVALID_LOTTO_SIZE.message } // Lotto has to be 6 numbers
     }
 
@@ -14,11 +11,11 @@ data class Lotto(private val numbers: Set<Int>) {
         return this.numbers.intersect(winningTicket.numbers).size
     }
 
-    fun containsBonus(bonusNumber: Int): Boolean {
+    fun containsBonus(bonusNumber: LottoNumber): Boolean {
         return numbers.contains(bonusNumber)
     }
 
-    fun getLottoNumbers(): Set<Int> {
+    fun getLottoNumbers(): Set<LottoNumber> {
         return numbers
     }
 }
